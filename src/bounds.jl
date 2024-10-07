56da073b0e7779521577b01c07aaa36631ca16c6
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-10-07 17:48:55 -0300

    fix_inf_bounds tambem corrige lower -inf

--- src/bounds.jl
+++ src/bounds.jl
@@ -4,9 +4,13 @@
 
 Change `Inf` upper bounds to `sub`, as `DualSDDP` doesn't allow unbounded variables
 """
-function fix_infinity_bounds!(md::ModelData, sub::Float64)
+function fix_infinity_bounds!(md::ModelData, sub_lower::Float64, sub_upper::Float64)
     for i in range(1, length(md.x_upper))
-        md.x_upper[i] = isinf(md.x_upper[i]) ? sub : md.x_upper[i]
+        md.x_upper[i] = isinf(md.x_upper[i]) ? sub_upper : md.x_upper[i]
+    end
+
+    for i in range(1, length(md.x_lower))
+        md.x_lower[i] = isinf(md.x_lower[i]) ? sub_lower : md.x_lower[i]
     end
 end
 
